7cbe12133cb66d14341a727267a6a45f70fd68ad
Joshardals <joshuabamidele219@gmail.com>
2024-01-06 19:47:20 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <div className="flex h-screen flex-col">
       <Header />
-      <main className="flex-1 mt-[9.5rem]">{children}</main>
+      <main className="flex-1 mt-[7rem] sm:mt-[9.5rem]">{children}</main>
       <Footer />
     </div>
   );
