80f9c118564f64e6bcd0b92f54b24abe1768ef97
Joshardals <joshuabamidele219@gmail.com>
2024-01-06 17:08:54 +0100

    initial commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <section className="container">
       <div
         className="flex items-center justify-between bg-gray-900
-        backdrop-blur-lg rounded-full p-5"
+        border border-gray-800 backdrop-blur-lg rounded-full p-5"
       >
         Footer - Light/Dark Mode, Sharing Buttons, Messaging Apps. Saving
         Options, maybe Inspiration Gallery ( on subsequent updates)
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="container fixed top-0 left-0 right-0 backdrop-blur-lg">
       <div
         className="flex items-center justify-between bg-gray-900
-       backdrop-blur-lg rounded-full p-5"
+       backdrop-blur-lg rounded-full p-5 border border-gray-800"
       >
         <Link href="/" className="flex items-center space-x-2">
           <Image
