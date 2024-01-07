27ce008af9b5716ed5b87de02141f1b7573c1d39
Joshardals <joshuabamidele219@gmail.com>
2024-01-08 00:08:41 +0100

    initial commit

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -5,14 +5,25 @@ import { Icons } from "../ui/icons";
 export default function Search() {
   const { search, setSearch } = useSearchStore();
   return (
-    <div
-      className={`bg-gray-700/50 py-2 px-4 flex items-center justify-between
-    cursor-pointer rounded-lg w-[15rem] hover:bg-gray-700/80`}
-      onClick={setSearch}
-    >
-      <Icons.search className="h-5 w-5" />
-      <p>Enter location</p>
-      <p>Ctrl K</p>
+    <div>
+      <div
+        className="bg-gray-700/50 py-2 px-4 flex items-center justify-between
+    cursor-pointer rounded-lg w-[15rem] hover:bg-gray-700/80 max-sm:hidden"
+        onClick={setSearch}
+      >
+        <Icons.search className="h-5 w-5" />
+        <p>Enter location</p>
+        <p>Ctrl K</p>
+      </div>
+
+      <div>
+        
+      </div>
+
+      {/* Mobile Device */}
+      <div className="sm:hidden" onClick={setSearch}>
+        <Icons.search className="h-5 w-5 cursor-pointer hover:text-white transitionAll" />
+      </div>
     </div>
   );
 }
--- components/shared/SearchItem.tsx
+++ components/shared/SearchItem.tsx
@@ -10,11 +10,23 @@ export default function SearchContainer() {
   return (
     <div
       className={`fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden
-    backdrop-blur-sm justify-centered  opacity-0
-    ${search && "opacity-100"}
+    backdrop-blur-sm justify-centered opacity-0
+    ${search && "opacity-100"} 
     `}
+      onClick={() => {
+        if (search) {
+          setSearch();
+        }
+      }}
     >
-      <div className="flex justify-center">
+      <div
+        className="flex justify-center"
+        onClick={(e) => {
+          if (search) {
+            e.stopPropagation(); 
+          }
+        }}
+      >
         <GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEO_API_KEY}>
           <div className=" sm:w-[45rem]">
             <GeoapifyGeocoderAutocomplete
