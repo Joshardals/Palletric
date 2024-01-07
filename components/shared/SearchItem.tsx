bbc760b592654927cb34e25d928017eb700405ff
Joshardals <joshuabamidele219@gmail.com>
2024-01-07 22:54:24 +0100

    initial commit

--- components/shared/SearchItem.tsx
+++ components/shared/SearchItem.tsx
@@ -1,23 +1,23 @@
-"use client";
-import {
-  GeoapifyGeocoderAutocomplete,
-  GeoapifyContext,
-} from "@geoapify/react-geocoder-autocomplete";
-import "@geoapify/geocoder-autocomplete/styles/round-borders-dark.css";
-export default function SearchContainer() {
-  return (
-    <div
-      className="fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden
-    backdrop-blur-sm justify-centered
-    "
-    >
-      <div className="flex justify-center">
-        <GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEO_API_KEY}>
-          <div className=" sm:w-[45rem]">
-            <GeoapifyGeocoderAutocomplete placeholder="Enter a location" />
-          </div>
-        </GeoapifyContext>
-      </div>
-    </div>
-  );
-}
+// "use client";
+// import {
+//   GeoapifyGeocoderAutocomplete,
+//   GeoapifyContext,
+// } from "@geoapify/react-geocoder-autocomplete";
+// import "@geoapify/geocoder-autocomplete/styles/round-borders-dark.css";
+// export default function SearchContainer() {
+//   return (
+//     <div
+//       className="fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden
+//     backdrop-blur-sm justify-centered
+//     "
+//     >
+//       <div className="flex justify-center">
+//         <GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEO_API_KEY}>
+//           <div className=" sm:w-[45rem]">
+//             <GeoapifyGeocoderAutocomplete placeholder="Enter a location" />
+//           </div>
+//         </GeoapifyContext>
+//       </div>
+//     </div>
+//   );
+// }
