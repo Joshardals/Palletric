c70d89792027d8b7f74edf85cd902a34d557a29e
Joshardals <joshuabamidele219@gmail.com>
2024-01-08 15:32:28 +0100

    iniital commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,35 +1,35 @@
-// "use client";
-// import SearchContainer from "@/components/shared/SearchItem";
-// import { useSearchStore } from "@/lib/store/store";
-// import { useEffect } from "react";
+"use client";
+import SearchContainer from "@/components/shared/SearchItem";
+import { useSearchStore } from "@/lib/store/store";
+import { useEffect } from "react";
 
-// export default function Home() {
-//   const { search, setSearch } = useSearchStore();
+export default function Home() {
+  const { search, setSearch } = useSearchStore();
 
-//   useEffect(() => {
-//     const handleKeyDown = (e: any) => {
-//       if (e.ctrlKey && e.key === "k") {
-//         setSearch;
-//         alert(search);
-//       }
-//       // if (search) {
-//       //   if (e.key === "Escape") {
-//       //     setSearch();
-//       //   }
-//       // }
-//     };
+  useEffect(() => {
+    const handleKeyDown = (e: any) => {
+      if (e.ctrlKey && e.key === "k") {
+        setSearch;
+        alert(search);
+      }
+      // if (search) {
+      //   if (e.key === "Escape") {
+      //     setSearch();
+      //   }
+      // }
+    };
 
-//     document.addEventListener("keydown", handleKeyDown);
-//   }, []);
+    document.addEventListener("keydown", handleKeyDown);
+  }, []);
 
-//   return (
-//     <main className="container">
-//       <div className="px-5">
-//         <p>What good!</p>
-//         <p>What good!</p>
-//         <p>What good!</p>
-//         {search && <SearchContainer />}
-//       </div>
-//     </main>
-//   );
-// }
+  return (
+    <main className="container">
+      <div className="px-5">
+        <p>What good!</p>
+        <p>What good!</p>
+        <p>What good!</p>
+        {search && <SearchContainer />}
+      </div>
+    </main>
+  );
+}
