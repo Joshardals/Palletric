944cb59f4d37aa289f3173af08d463c21eb0f00c
Joshardals <joshuabamidele219@gmail.com>
2024-01-08 17:56:14 +0100

    iniital commit

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -8,22 +8,18 @@ export default function Search() {
     <div>
       <div
         className="bg-gray-700/50 py-2 px-4 flex items-center justify-between
-    cursor-pointer rounded-lg w-[15rem] hover:bg-gray-700/80 max-sm:hidden"
+    cursor-pointer rounded-lg max-md:space-x-2 md:w-[15rem] hover:bg-gray-700/80"
         onClick={setSearch}
       >
         <Icons.search className="h-5 w-5" />
         <p>Enter location</p>
-        <p>Ctrl K</p>
-      </div>
-
-      <div>
-        
+        <p className="max-md:hidden">Ctrl K</p>
       </div>
 
       {/* Mobile Device */}
-      <div className="sm:hidden" onClick={setSearch}>
+      {/* <div className="sm:hidden" onClick={setSearch}>
         <Icons.search className="h-5 w-5 cursor-pointer hover:text-white transitionAll" />
-      </div>
+      </div> */}
     </div>
   );
 }
