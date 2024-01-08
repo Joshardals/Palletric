82a60a2fff1b42cdc809c0db643d3a7f472c7e97
Joshardals <joshuabamidele219@gmail.com>
2024-01-08 15:31:20 +0100

    iniital commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,26 +1,26 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import Search from "./Search";
+import Image from "next/image";
+import Link from "next/link";
+import Search from "./Search";
 
-// export default function Header() {
-//   return (
-//     <div className="container fixed top-0 left-0 right-0 backdrop-blur-lg select-none">
-//       <div
-//         className="flex items-center justify-between bg-gray-900
-//        backdrop-blur-lg rounded-2xl p-5 border border-gray-800"
-//       >
-//         <Link href="/" className="flex items-center space-x-2">
-//           <Image
-//             src="/assets/logo.png"
-//             alt="Palletric Logo"
-//             width={30}
-//             height={30}
-//             className=""
-//           />
-//           <h1 className="h1-bold">Palletric</h1>
-//         </Link>
-//         <Search />
-//       </div>
-//     </div>
-//   );
-// }
+export default function Header() {
+  return (
+    <div className="container fixed top-0 left-0 right-0 backdrop-blur-lg select-none">
+      <div
+        className="flex items-center justify-between bg-gray-900
+       backdrop-blur-lg rounded-2xl p-5 border border-gray-800"
+      >
+        <Link href="/" className="flex items-center space-x-2">
+          <Image
+            src="/assets/logo.png"
+            alt="Palletric Logo"
+            width={30}
+            height={30}
+            className=""
+          />
+          <h1 className="h1-bold">Palletric</h1>
+        </Link>
+        <Search />
+      </div>
+    </div>
+  );
+}
--- components/shared/SearchItem.tsx
+++ components/shared/SearchItem.tsx
@@ -1,41 +1,41 @@
-"use client";
-import {
-  GeoapifyGeocoderAutocomplete,
-  GeoapifyContext,
-} from "@geoapify/react-geocoder-autocomplete";
-import "@geoapify/geocoder-autocomplete/styles/round-borders-dark.css";
-import { useSearchStore } from "@/lib/store/store";
-export default function SearchContainer() {
-  const { search, setSearch } = useSearchStore();
-  return (
-    <div
-      className={`fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden
-    backdrop-blur-sm justify-centered opacity-0
-    ${search && "opacity-100"} 
-    `}
-      onClick={() => {
-        if (search) {
-          setSearch();
-        }
-      }}
-    >
-      <div
-        className="flex justify-center"
-        onClick={(e) => {
-          if (search) {
-            e.stopPropagation(); 
-          }
-        }}
-      >
-        <GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEO_API_KEY}>
-          <div className=" sm:w-[45rem]">
-            <GeoapifyGeocoderAutocomplete
-              placeholder="Enter a location"
-              skipIcons
-            />
-          </div>
-        </GeoapifyContext>
-      </div>
-    </div>
-  );
-}
+// "use client";
+// import {
+//   GeoapifyGeocoderAutocomplete,
+//   GeoapifyContext,
+// } from "@geoapify/react-geocoder-autocomplete";
+// import "@geoapify/geocoder-autocomplete/styles/round-borders-dark.css";
+// import { useSearchStore } from "@/lib/store/store";
+// export default function SearchContainer() {
+//   const { search, setSearch } = useSearchStore();
+//   return (
+//     <div
+//       className={`fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden
+//     backdrop-blur-sm justify-centered opacity-0
+//     ${search && "opacity-100"} 
+//     `}
+//       onClick={() => {
+//         if (search) {
+//           setSearch();
+//         }
+//       }}
+//     >
+//       <div
+//         className="flex justify-center"
+//         onClick={(e) => {
+//           if (search) {
+//             e.stopPropagation(); 
+//           }
+//         }}
+//       >
+//         <GeoapifyContext apiKey={process.env.NEXT_PUBLIC_GEO_API_KEY}>
+//           <div className=" sm:w-[45rem]">
+//             <GeoapifyGeocoderAutocomplete
+//               placeholder="Enter a location"
+//               skipIcons
+//             />
+//           </div>
+//         </GeoapifyContext>
+//       </div>
+//     </div>
+//   );
+// }
