23cad3e5cfca4e6a4cdbf88e742f1c48df705f48
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 00:28:58 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,11 @@ import Header from "@/components/shared/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col h-screen max-md:overflow-auto">
+    <div className="flex flex-col h-screen max-md:overflow-auto bg-red-500">
       <Header />
-      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem]">{children}</main>
+      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem] bg-blue-500">
+        {children}
+      </main>
       <Footer />
     </div>
   );
