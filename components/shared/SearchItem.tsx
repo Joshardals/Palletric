cd58173b90839744fadff120316399fcaa6da9d6
Joshardals <joshuabamidele219@gmail.com>
2024-01-10 21:49:44 +0100

    initial commit

--- components/shared/SearchItem.tsx
+++ components/shared/SearchItem.tsx
@@ -201,7 +201,7 @@ export default function SearchContainer() {
 
   return (
     <div
-      className={`absolute top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden select-none
+      className={`fixed top-0 left-0 h-screen w-full bg-gray-900/70 z-10 overflow-hidden select-none
     backdrop-blur-sm justify-centered opacity-0 p-5 sm:px-20
     ${search && "opacity-100"} 
     `}
