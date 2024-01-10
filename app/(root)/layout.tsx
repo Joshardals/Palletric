ff322250d7e6d257bfc4e1a84bf04c014c0e21f4
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 00:42:33 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,26 +4,10 @@ import Header from "@/components/shared/Header";
 import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const handleResize = () => {
-      // Prevent default behavior on resize
-      if (window.innerHeight !== window.screen.height) {
-        window.scrollTo(0, 1);
-      }
-    };
-  
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  
   return (
     <div className="flex flex-col">
       <Header />
-      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem]">
-        {children}
-      </main>
+      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem]">{children}</main>
       <Footer />
     </div>
   );
