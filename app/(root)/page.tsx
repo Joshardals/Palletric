2ef1b5fbc282f9ead28f152b8047dfd15aa62a62
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 18:09:00 +0100

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -47,7 +47,33 @@ export default function Home() {
         <p>What good!</p>
         <p>What good!</p>
 
-        {search && <SearchContainer />}
+        {/* {search && <SearchContainer />} */}
+
+        <div
+          className={`bg-gray-800/40 fixed top-0 left-0  box-border h-full w-full flex items-center justify-center opacity-0 pointer-events-none transition-opacity duration-300
+          ${search ? " opacity-100 pointer-events-auto" : ""}
+        `}
+        >
+          <div className="space-y-4 bg-gray-900 p-5 text-center w-[40rem] max-w-full">
+            <div>
+              <p>Hey there, what is popping??</p>
+
+              <input
+                type="text"
+                placeholder="Search for a place"
+                className="appearance-none outline-none bg-yellow-950"
+                autoFocus
+              />
+            </div>
+
+            <button
+              className="px-4 py-2 bg-orange-600 rounded-full font-bold"
+              onClick={setSearch}
+            >
+              Close Modal
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   );
