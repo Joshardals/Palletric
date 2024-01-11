f9b2519d4977f387c2de58c775b8bbfbc2bff59d
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 19:33:29 +0100

    initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,12 +10,6 @@ export default function Home() {
   // Event Listeners for ctrl + k and esc button -- START.
 
   useEffect(() => {
-    const handleTouchMove = (e: TouchEvent<HTMLDivElement>) => {
-      if (search) {
-        e.preventDefault();
-      }
-    };
-
     const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
       if (e.ctrlKey && e.key === "k") {
         e.preventDefault();
@@ -29,13 +23,9 @@ export default function Home() {
       }
     };
 
-    document.body.addEventListener("touchmove", handleTouchMove as any, {
-      passive: false,
-    });
     document.addEventListener("keydown", handleKeyDown as any);
 
     return () => {
-      document.body.removeEventListener("touchmove", handleTouchMove as any);
       document.removeEventListener("keydown", handleKeyDown as any);
     };
   }, []);
@@ -43,7 +33,7 @@ export default function Home() {
   // Event Listeners for ctrl + k and esc button -- END.
 
   useEffect(() => {
-    document.body.classList.toggle("oveerflow-hidden", search);
+    document.body.classList.toggle("overflow-hidden", search);
 
     return () => {
       document.body.classList.remove("overflow-hidden");
@@ -58,33 +48,32 @@ export default function Home() {
         <p>What good!</p>
 
         {/* {search && <SearchContainer />} */}
-        {search && (
-          <div
-            className={`bg-gray-800/40 p-10 fixed overflow-hidden top-0 left-0 bottom-0 box-border h-screen w-screen flex items-center justify-center opacity-0 transition-opacity duration-300
-            ${search ? " opacity-100" : ""}
+
+        <div
+          className={`bg-gray-800/40 p-10 fixed overflow-hidden top-0 left-0 bottom-0 box-border h-screen w-screen flex items-center justify-center opacity-0 pointer-events-none transition-opacity duration-300
+            ${search ? " opacity-100 pointer-events-auto" : ""}
         `}
-          >
-            <div className="space-y-4 bg-gray-900 p-5 text-center sm:w-[40rem] max-w-full">
-              <div>
-                <p>Hey there, what is popping??</p>
+        >
+          <div className="space-y-4 bg-gray-900 p-5 text-center sm:w-[40rem] max-w-full">
+            <div>
+              <p>Hey there, what is popping??</p>
 
-                <input
-                  type="text"
-                  placeholder="Search for a place"
-                  className="appearance-none outline-none bg-yellow-950"
-                  autoFocus
-                />
-              </div>
-
-              <button
-                className="px-4 py-2 bg-orange-600 rounded-full font-bold"
-                onClick={setSearch}
-              >
-                Close Modal
-              </button>
+              <input
+                type="text"
+                placeholder="Search for a place"
+                className="appearance-none outline-none bg-yellow-950"
+                autoFocus
+              />
             </div>
+
+            <button
+              className="px-4 py-2 bg-orange-600 rounded-full font-bold"
+              onClick={setSearch}
+            >
+              Close Modal
+            </button>
           </div>
-        )}
+        </div>
       </div>
     </main>
   );
