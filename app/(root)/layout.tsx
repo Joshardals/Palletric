758c23831867d10b8432b4e1eb69b825a33da776
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 02:00:24 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+"use client";
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import { useSearchStore } from "@/lib/store/store";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { search } = useSearchStore();
   return (
-    <div className="flex flex-col h-full max-md:overflow-auto">
+    <div
+      className={`flex flex-col h-full max-md:overflow-auto ${
+        search && "fixed"
+      }`}
+    >
       {/* <Header /> */}
-      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem]">{children}</main>
+      <main className="flex-1 mt-[7.2rem] sm:mt-[10rem}">{children}</main>
       <Footer />
     </div>
   );
