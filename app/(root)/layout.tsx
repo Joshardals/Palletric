3bac5e49270baad1c0351cd3f4ab2de1e5f870da
Joshardals <joshuabamidele219@gmail.com>
2024-01-11 02:22:52 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,31 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 import { useSearchStore } from "@/lib/store/store";
+import { useEffect, useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { search } = useSearchStore();
+  // Inside your SearchContainer component
+  const [isVirtualKeyboardActive, setVirtualKeyboardActive] = useState(false);
+
+  // Inside your useEffect that sets up event listeners
+  useEffect(() => {
+    const handleResize = () => {
+      // Check if the viewport height has changed
+      setVirtualKeyboardActive(window.innerHeight < window.screen.height);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div
       className={`flex flex-col h-full max-md:overflow-auto ${
-        search && " overflow-hidden"
+        (search || isVirtualKeyboardActive) && " overflow-hidden"
       }`}
     >
       {/* <Header /> */}
