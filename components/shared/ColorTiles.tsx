9d50b69f51390e88e0581e84442119e94a1e94e8
Joshardals <joshuabamidele219@gmail.com>
2024-01-14 00:16:42 +0100

    initial commit

--- components/shared/ColorTiles.tsx
+++ components/shared/ColorTiles.tsx
@@ -8,7 +8,7 @@ export default function ColorTiles() {
         <div className=" bg-orange-950 rounded-md flex-1" />
         <div className="text-center p-1">color code</div>
       </div>
-      {/* Where I will display the color codes on mobile devices */}s
+      {/* Where I will display the color codes on mobile devices */}
     </div>
   );
 }
