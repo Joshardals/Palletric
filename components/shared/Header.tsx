ab604eb73ec4143f732fc73e37a2e986c0088665
Joshardals <joshuabamidele219@gmail.com>
2024-01-15 17:31:31 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 left-0 right-0 backdrop-blur-lg select-none hfPadding z-10">
+    <div className="fixed top-0 left-0 right-0 backdrop-blur-[100px] select-none hfPadding z-10">
       <div
         className="flex items-center justify-between bg-gray-900
        backdrop-blur-lg rounded-2xl p-5 border border-gray-800"
