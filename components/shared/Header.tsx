90052bd35e31a2e3913df93d40b91821ae4ee275
Joshardals <joshuabamidele219@gmail.com>
2024-01-15 17:32:48 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,26 +1,26 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import Search from "./Search";
+import Image from "next/image";
+import Link from "next/link";
+import Search from "./Search";
 
-// export default function Header() {
-//   return (
-//     <div className="fixed top-0 left-0 right-0 backdrop-blur-[100px] select-none hfPadding z-10">
-//       <div
-//         className="flex items-center justify-between bg-gray-900
-//        backdrop-blur-lg rounded-2xl p-5 border border-gray-800"
-//       >
-//         <Link href="/" className="flex items-center space-x-2">
-//           <Image
-//             src="/assets/logo.png"
-//             alt="Palletric Logo"
-//             width={30}
-//             height={30}
-//             className=" max-sm:h-6 max-sm:w-6"
-//           />
-//           <h1 className="h1-bold font-sans ">palletric</h1>
-//         </Link>
-//         <Search />
-//       </div>
-//     </div>
-//   );
-// }
+export default function Header() {
+  return (
+    <div className="fixed top-0 left-0 right-0 backdrop-blur-[100px] select-none hfPadding z-10">
+      <div
+        className="flex items-center justify-between bg-gray-900
+       backdrop-blur-lg rounded-2xl p-5 border border-gray-800"
+      >
+        <Link href="/" className="flex items-center space-x-2">
+          <Image
+            src="/assets/logo.png"
+            alt="Palletric Logo"
+            width={30}
+            height={30}
+            className=" max-sm:h-6 max-sm:w-6"
+          />
+          <h1 className="h1-bold font-sans ">palletric</h1>
+        </Link>
+        <Search />
+      </div>
+    </div>
+  );
+}
