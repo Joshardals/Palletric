3b0601d01a02e17737722f87c1cce179b9ff7876
Joshardals <joshuabamidele219@gmail.com>
2024-01-18 16:28:02 +0100

    initial commit

--- components/shared/Palettes.tsx
+++ components/shared/Palettes.tsx
@@ -1,57 +1,57 @@
-"use client";
-import {
-  useBrightness,
-  useHue,
-  usePaletteStore,
-  useSaturation,
-} from "@/lib/store/store";
-import ColorControls from "./ColorControls";
-import ColorTiles from "./ColorTiles";
-import { useState } from "react";
+// "use client";
+// import {
+//   useBrightness,
+//   useHue,
+//   usePaletteStore,
+//   useSaturation,
+// } from "@/lib/store/store";
+// import ColorControls from "./ColorControls";
+// import ColorTiles from "./ColorTiles";
+// import { useState } from "react";
 
-const colors = [
-  { id: 1, color: "#9A4DFF" },
-  { id: 2, color: "#F27300" },
-  { id: 3, color: "#5AC8FA" },
-  { id: 4, color: "#FFD700" },
-  { id: 5, color: "#4CAF50" },
-  { id: 6, color: "#FF4081" },
-];
+// const colors = [
+//   { id: 1, color: "#9A4DFF" },
+//   { id: 2, color: "#F27300" },
+//   { id: 3, color: "#5AC8FA" },
+//   { id: 4, color: "#FFD700" },
+//   { id: 5, color: "#4CAF50" },
+//   { id: 6, color: "#FF4081" },
+// ];
 
-export default function Palettes() {
-  const { brightness } = useBrightness();
-  const { saturation } = useSaturation();
-  const { hue } = useHue();
-  const { palette, updatePalette } = usePaletteStore();
-  // const [palette, setPalette] = useState(colors);
+// export default function Palettes() {
+//   const { brightness } = useBrightness();
+//   const { saturation } = useSaturation();
+//   const { hue } = useHue();
+//   const { palette, updatePalette } = usePaletteStore();
+//   // const [palette, setPalette] = useState(colors);
 
-  return (
-    <section className="w-full space-y-10">
-      {/* Options inlcudes: Color Palettes, Inspired Palettes, Explore Hues, Location-Inspired Palettes */}
-      <div className="space-y-2">
-        <h1 className="h1-bold text-center font-bold">Explore Your Palette</h1>
-        {/* I might consider tweaking this subtitle, by animating others in I guess
-          Sub-heading:
-          Inspired by Nature's Colors., Local Hues, Global Inspiration, Palette Inspiration Awaits, From Location to Color Symphony
-        */}
-        <p className="capitalize text-center">Your location, your palette</p>
-      </div>
+//   return (
+//     <section className="w-full space-y-10">
+//       {/* Options inlcudes: Color Palettes, Inspired Palettes, Explore Hues, Location-Inspired Palettes */}
+//       <div className="space-y-2">
+//         <h1 className="h1-bold text-center font-bold">Explore Your Palette</h1>
+//         {/* I might consider tweaking this subtitle, by animating others in I guess
+//           Sub-heading:
+//           Inspired by Nature's Colors., Local Hues, Global Inspiration, Palette Inspiration Awaits, From Location to Color Symphony
+//         */}
+//         <p className="capitalize text-center">Your location, your palette</p>
+//       </div>
 
-      <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
-        {palette.map((color, index) => (
-          <ColorTiles
-            key={index}
-            id={index}
-            color={color}
-            brightness={brightness}
-            saturation={saturation}
-            hue={hue}
-            index={index}
-          />
-        ))}
-      </div>
+//       <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
+//         {palette.map((color, index) => (
+//           <ColorTiles
+//             key={index}
+//             id={index}
+//             color={color}
+//             brightness={brightness}
+//             saturation={saturation}
+//             hue={hue}
+//             index={index}
+//           />
+//         ))}
+//       </div>
 
-      <ColorControls />
-    </section>
-  );
-}
+//       <ColorControls />
+//     </section>
+//   );
+// }
