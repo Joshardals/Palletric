dc265a57932a5961311573ceaf4efb2d89b71f35
Joshardals <joshuabamidele219@gmail.com>
2024-01-21 13:35:28 +0100

    initial commit

--- components/shared/Palettes.tsx
+++ components/shared/Palettes.tsx
@@ -7,23 +7,13 @@ import {
 } from "@/lib/store/store";
 import ColorControls from "./ColorControls";
 import ColorTiles from "./ColorTiles";
-import { useState } from "react";
 
-const colors = [
-  { id: 1, color: "#9A4DFF" },
-  { id: 2, color: "#F27300" },
-  { id: 3, color: "#5AC8FA" },
-  { id: 4, color: "#FFD700" },
-  { id: 5, color: "#4CAF50" },
-  { id: 6, color: "#FF4081" },
-];
 
 export default function Palettes() {
   const { brightness } = useBrightness();
   const { saturation } = useSaturation();
   const { hue } = useHue();
   const { palette, updatePalette } = usePaletteStore();
-  // const [palette, setPalette] = useState(colors);
 
   return (
     <section className="w-full space-y-10">
