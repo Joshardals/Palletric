9717012c0dfac91f71da66b6e57c527201c0cefd
Joshardals <joshuabamidele219@gmail.com>
2024-01-22 21:02:58 +0100

    initial commit

--- components/shared/Palettes.tsx
+++ components/shared/Palettes.tsx
@@ -1,66 +1,66 @@
-"use client";
-import {
-  useBrightness,
-  useHue,
-  usePaletteStore,
-  useSaturation,
-} from "@/lib/store/store";
-import ColorControls from "./ColorControls";
-import ColorTiles from "./ColorTiles";
-import Skeleton from "react-loading-skeleton";
+// "use client";
+// import {
+//   useBrightness,
+//   useHue,
+//   usePaletteStore,
+//   useSaturation,
+// } from "@/lib/store/store";
+// import ColorControls from "./ColorControls";
+// import ColorTiles from "./ColorTiles";
+// import Skeleton from "react-loading-skeleton";
 
-export default function Palettes() {
-  const { brightness } = useBrightness();
-  const { saturation } = useSaturation();
-  const { hue } = useHue();
-  const { palette } = usePaletteStore();
+// export default function Palettes() {
+//   const { brightness } = useBrightness();
+//   const { saturation } = useSaturation();
+//   const { hue } = useHue();
+//   const { palette } = usePaletteStore();
 
-  return (
-    <section className="w-full space-y-10">
-      {/* Options inlcudes: Color Palettes, Inspired Palettes, Explore Hues, Location-Inspired Palettes */}
-      <div className="space-y-2">
-        <h1 className="h1-bold text-center font-bold exploreBG">
-          Explore Your Palette
-        </h1>
-        {/* I might consider tweaking this subtitle, by animating others in I guess
-          Sub-heading:
-          Inspired by Nature's Colors., Local Hues, Global Inspiration, Palette Inspiration Awaits, From Location to Color Symphony
-        */}
-        <p className="capitalize text-center">Your location, your palette</p>
-      </div>
+//   return (
+//     <section className="w-full space-y-10">
+//       {/* Options inlcudes: Color Palettes, Inspired Palettes, Explore Hues, Location-Inspired Palettes */}
+//       <div className="space-y-2">
+//         <h1 className="h1-bold text-center font-bold exploreBG">
+//           Explore Your Palette
+//         </h1>
+//         {/* I might consider tweaking this subtitle, by animating others in I guess
+//           Sub-heading:
+//           Inspired by Nature's Colors., Local Hues, Global Inspiration, Palette Inspiration Awaits, From Location to Color Symphony
+//         */}
+//         <p className="capitalize text-center">Your location, your palette</p>
+//       </div>
 
-      {palette.length > 0 ? (
-        <div className="space-y-10">
-          <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
-            {palette.map((color, index) => (
-              <ColorTiles
-                key={index}
-                id={index}
-                color={color}
-                brightness={brightness}
-                saturation={saturation}
-                hue={hue}
-                index={index}
-              />
-            ))}
-          </div>
+//       {palette.length > 0 ? (
+//         <div className="space-y-10">
+//           <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
+//             {palette.map((color, index) => (
+//               <ColorTiles
+//                 key={index}
+//                 id={index}
+//                 color={color}
+//                 brightness={brightness}
+//                 saturation={saturation}
+//                 hue={hue}
+//                 index={index}
+//               />
+//             ))}
+//           </div>
 
-          <ColorControls />
-        </div>
-      ) : (
-        <div className="space-y-10">
-          <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
-            {Array.from({ length: 6 }).map((_, index) => (
-              <Skeleton
-                key={index}
-                baseColor="#1a202c"
-                highlightColor="#2d3748"
-                className=" max-sm:h-[5rem] h-[10rem] rounded-md"
-              />
-            ))}
-          </div>
-        </div>
-      )}
-    </section>
-  );
-}
+//           <ColorControls />
+//         </div>
+//       ) : (
+//         <div className="space-y-10">
+//           <div className=" grid grid-cols-6 gap-8 max-md:grid-cols-2 content-center">
+//             {Array.from({ length: 6 }).map((_, index) => (
+//               <Skeleton
+//                 key={index}
+//                 baseColor="#1a202c"
+//                 highlightColor="#2d3748"
+//                 className=" max-sm:h-[5rem] h-[10rem] rounded-md"
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </section>
+//   );
+// }
